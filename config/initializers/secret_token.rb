4c6158b37a4fa5d68381d6a93d4dc1d770bec9c3
David Goldberg <drgold1988@gmail.com>
2015-11-19 10:03:08 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EXange::Application.config.secret_key_base = '0100b8192707e13d300e057579c79b8fe3f05b3e2311838e6d7e018a6ec768df791e11b80a4cf14707efc41608a2e6bf1b76ccc2106c8f85b35135fe55b29d10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EXange::Application.config.session_store :cookie_store, key: '_eXange_session'
